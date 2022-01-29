9f6acacf568d857b824d8573e4b7fb42577202d2
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-01-29 15:59:47 +0530

    Rotate Matrix By 90 degree

---
+++ Serious/Matrix/rotate_matrix_by_90_degree.py
@@ -0,0 +1,9 @@
+def rotate_matrix(matrix):
+    for i in range(0,len(matrix)):
+        for j in range(0,len(matrix[0])):
+            if i != j :
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j] # Transpose Of matrix created
+    
+    for i in matrix:
+        i.reverse()
+    print(matrix)
