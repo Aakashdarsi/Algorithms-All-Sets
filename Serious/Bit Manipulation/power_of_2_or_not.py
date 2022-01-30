0dc34bbfcb051e80ef622d36b2371eaf0377c4e6
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-01-30 12:45:35 +0530

    Power of 2

---
+++ Serious/Bit Manipulation/power_of_2_or_not.py
@@ -0,0 +1,15 @@
+# https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1#
+# Bit Fiddling Technique n&(n-1) if result is zero then its a power of 2 otherwise its not a power of 2
+
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self,n):
+        ##Your code here
+        if n == 0:
+            return False
+        else:
+            if( n &(n-1)) != 0:
+                return False
+            else:
+                return True
