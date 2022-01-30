691dbb5986edd9c0caf83ce3628987391b4d5d49
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-01-30 14:55:47 +0530

    Calculate square of a number without using *, / and pow()

---
+++ Serious/Bit Manipulation/square_of_num_without_oper.py
@@ -0,0 +1,13 @@
+#https://www.geeksforgeeks.org/calculate-square-of-a-number-without-using-and-pow/
+#https://www.youtube.com/watch?v=pOKuLSKnAW4
+n = 5
+if n <0:
+    n = -n 
+temp = n 
+count = 0 
+res = 0 
+while temp !=0 :
+    if temp&1 ==1:
+        res+=n<<count 
+    count +=1 
+    temp = temp>>1
