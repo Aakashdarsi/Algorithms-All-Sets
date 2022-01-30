780668a0b5dec0957dfc9769d3b069e234e096f5
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2022-01-30 13:19:29 +0530

    Update power_of_2_or_not.py

--- Serious/Bit Manipulation/power_of_2_or_not.py
+++ Serious/Bit Manipulation/power_of_2_or_not.py
@@ -1,5 +1,5 @@
 # https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1#
-# Bit Fiddling Technique n&(n-1) if result is zero then its a power of 2 otherwise its not a power of 2
+# Bit Fiddling Technique n&(n-1) if result is zero then its a power of 2 otherwise its not a power of 2 .Very Important technique in Bit manipulation
 #https://www.youtube.com/watch?v=YZRlBjyUnKU
 class Solution:
     ##Complete this function
