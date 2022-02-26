16cca129d98c906c57af836a8330e4d5152e4215
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-02-26 10:46:00 +0530

    Subset Sum DP

---
+++ DSA-Python/Dynamic Programming/subset_sum_dp.py
@@ -0,0 +1,22 @@
+set_ele = list(map(int,input().split()))
+sum_to = int(input())
+n = len(set_ele)
+dp = [[False for i in range(sum_to+1)]for j in range(n+1)]
+for i in range(n+1):
+    dp[i][0] = True # Number of elements to be zero or non zero but sum to be found is zero 
+for i in range(1,sum_to): # sum is non zero number of elements  are zero then False
+    dp[0][i] = False
+
+
+for i in range(1,n+1):
+    for j in range(1,sum_to+1):
+        if set_ele[i-1]>j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = dp[i-1][j] or dp[i-1][j-set_ele[i-1]]
+print(dp[n][sum_to])
+print("DP TABLE")
+for i in range(n+1):
+    for j in range(sum_to+1):
+        print(dp[i][j],end="| ")
+    print()
