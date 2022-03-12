2b14d04012fa98de657d84a2db0f9daddaa938f4
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-03-13 00:40:41 +0530

    Array List added

---
+++ javaprograms/arraylist.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>(20);
+        a.add(22);
+        a.add(33);
+        System.out.println(a);
+    }
+}
