8534f7feb801ef076398fb484dd891a6c62d5637
Aakash Darsi <darsisriguruaakash@gmail.com>
2022-04-02 21:51:13 +0530

    First Duplicat Ocuurence

---
+++ DSA-Python/AlgoExpert Easy/first_duplicate_occurence_in_array.py
@@ -0,0 +1,14 @@
+# [1,2,3,1,4,5,6,7]
+
+# Here 1 is the first duplicate element 
+# Approach 1
+def duplicate_first(array):
+    for i in range(len(array)-1):
+        for j in range(i+1,len(array)):
+            if array[i] == array[j]:
+                print(array[i],"is first duplicate")
+                return 
+    return "NO duplicates"
+
+l = list(map(int,input().split(',')))
+duplicate_first(l)
